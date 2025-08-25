448823356ea1cd2e36cb72a797e416a3fbd5ba77
Dr. Dean von Schoultz <deanvons@gmail.com>
2025-08-25 19:39:28 +0200

    FEAT: added unrefined CRUD

--- src/main/java/no/loop/hibernation/demo/runners/PostGradManagerRunner.java
+++ src/main/java/no/loop/hibernation/demo/runners/PostGradManagerRunner.java
@@ -19,16 +19,42 @@ public class PostGradManagerRunner implements CommandLineRunner {
     public void run(String... args) throws Exception {
         System.out.println("running");
 
-        // need to interact with the data
-
+        // need to interact with the data - CRUD
         // register some student
+
+        // CREATE
         Student newStudent = new Student();
         newStudent.setName("Dean");
-
         studentRepository.save(newStudent);
 
-//        how ?
-//        we need a repository look up top, DI
+        //READ
+        Student pulledStudent = studentRepository.findById(1).get();
+        System.out.println(pulledStudent.getName());
+
+        // UPDATE
+        pulledStudent.setName("Dr Dean");
+        studentRepository.save(pulledStudent);
+
+        // Check if updated correctly
+        System.out.println(pulledStudent.getName());
+
+        // DELETE
+
+
+        studentRepository.delete(pulledStudent);
+
+        System.out.println(pulledStudent.getName()); // note the student is still in memory here
+
+        // Student pulledStudentTest = studentRepository.findById(1).get(); // fails need to check if it exists -> Optional. get()' without 'isPresent()' check
+
+
+        if (!studentRepository.existsById(pulledStudent.getId())) {
+            studentRepository.delete(pulledStudent);
+            System.out.println("deleted");
+        } else {
+            System.out.println("not found");
+        }
+
 
     }
 }
